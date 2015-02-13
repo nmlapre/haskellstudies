bd8ae6ca09b4e7790d05b4bb6eba169a19de4a11
Nathan LaPre <nmlapre@gmail.com>
2015-02-12 23:06:42 -0800

    evening work 2/12

--- realworldhaskell/ch04/Fold.hs
+++ realworldhaskell/ch04/Fold.hs
@@ -10,3 +10,17 @@ identity xs = foldr (:) [] xs
 
 append :: [a] -> [a] -> [a]
 append xs ys = foldr (:) ys xs
+
+-- incorrect: seq is hidden by the application of someFunc
+-- since someFunc will be evaluated first, seq may occur too late
+hiddenInside x y = someFunc (x `seq` y)
+
+-- incorrect: a variation of the above mistake
+hiddenByLet x y z = let a = x `seq` someFunc y
+                    in anotherFunc a z
+
+-- correct: seq will be evaluated first, forcing evaluation of x
+onTheOutside x y = x `seq` someFunc y
+
+-- to strictly evaluate several values, chain applications of seq together
+chained x y z = x `seq` y `seq` someFunc z
--- realworldhaskell/ch04/SuffixTree.hs
+++ realworldhaskell/ch04/SuffixTree.hs
@@ -4,11 +4,24 @@
 -- the wildcard _ indicates that we're not interested in the value of the head
 -- if the pattern after the @ matches, xs will be bound to the entire list that matched,
 -- and xs' to all but the head of the list.
+import Data.List -- for tails
 suffixes :: [a] -> [[a]]
-suffixes xs@(_xs') = xs : suffixes xs'
+suffixes xs@(_:xs') = xs : suffixes xs'
 suffixes _         = []
 
 -- this version doesn't use the as pattern, and as a result is less readable
 noAsPattern :: [a] -> [[a]]
 noAsPAttern (x:xs) = (x:xs) : noAsPattern xs
 noAsPattern _      = []
+
+-- init returns all but the last element of a list, so it can strip epsilon for us....nice
+suffixes2 xs = init (tails xs)
+
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g x = f (g x)
+
+suffixes3 xs = compose init tails xs
+
+suffxes4 = compose init tails
+
+suffixes5 = init . tails
